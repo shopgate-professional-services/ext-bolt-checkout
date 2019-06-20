4135955c37ca625231db7c9981c587547dbc721b
Daniel Quintanilla <daniel.quintanilla@shopgate.com>
2019-06-20 14:45:06 -0500

    CCP-1559: Mounting bolt-connect script on app start

--- frontend/subscriptions/index.js
+++ frontend/subscriptions/index.js
@@ -1,13 +1,17 @@
-import { main$ } from '@shopgate/pwa-common/streams/main';
+import { appDidStart$ } from '@shopgate/pwa-common/streams/app';
+import getConfig from '../helpers/getConfig';
+import injectBoltConnect from '../helpers/injectBoltConnect';
+
+const config = getConfig();
 
 /**
- * Subscriptions
- * @param {Function} subscribe Subscribe.
+ * Will mount bolt tracking and connect scripts on app start
+ * @param {Function} subscribe subscriber function
  */
-const dummySubscriptions = (subscribe) => {
-  subscribe(main$, ({ dispatch, getState }) => {
-
+const mountBoltScriptSubscription = (subscribe) => {
+  subscribe(appDidStart$, () => {
+    injectBoltConnect(config);
   });
 };
 
-export default dummySubscriptions;
+export default mountBoltScriptSubscription;
