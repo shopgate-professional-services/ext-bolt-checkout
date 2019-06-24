8a5ca566cc51394a3cb7a7dfb99347a4bfe2b57a
Carsten Schneider <carsten.schneider@shopgate.com>
2019-06-24 15:45:35 +0200

    CCP-1567 create bolt cart pipeline WIP

---
+++ extension/lib/buildBoltCart.js
@@ -0,0 +1,48 @@
+/**
+ * @param {PipelineContext} context
+ * @param {Object} input
+ * @returns {Promise<Object>}
+ */
+module.exports = async function (context, input) {
+  console.warn(JSON.stringify(input))
+
+  if (!input.cartItems.length) {
+    throw new Error('Your cart is empty')
+  }
+
+  const cartItems = input.cartItems.filter(({ type }) => type === 'product')
+
+  const items = cartItems.map((cartItem) => {
+    const product = cartItem.product
+
+    return {
+      name: product.name,
+      reference: cartItem.id,
+      total_amount: product.price.unit * cartItem.quantity * 100,
+      unit_price: product.price.unit * 100,
+      quantity: cartItem.quantity,
+      image_url: product.featuredImageUrl ? product.featuredImageUrl + '?w=440&h=440' : undefined,
+      // TODO: we don't have the key of the prop. we only have the label
+      properties: product.properties.map(({ label: key, value }) => ({ key, value }))
+    }
+  })
+
+  const tax = input.totals.find(({ type }) => type === 'tax')
+
+  return {
+    cart: {
+      order_reference: input.cartId,
+      currency: input.currency,
+      total_amount: input.totals.find(({ type }) => type === 'grandTotal').amount * 100,
+      tax_amount: tax ? tax.amount * 100 : undefined,
+      items
+      /*
+      discounts: input.appliedDiscounts.map((discount) => ({
+        amount: 1000, // TODO: how to handle percentage here
+        description: discount.description,
+        reference: discount.code,
+      }))
+      */
+    }
+  }
+}
--- extension/lib/transferCart.js
+++ extension/lib/transferCart.js
@@ -4,5 +4,12 @@
  * @returns {Promise<Object>}
  */
 module.exports = async function (context, input) {
-  return {}
+
+  console.warn(JSON.stringify(input.cart));
+
+  // TODO: request bolt API
+
+  return {
+    cartToken: '1234'
+  }
 }
