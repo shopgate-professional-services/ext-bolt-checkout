acb1fae3c801dbe8c6926c80f2a6075320bbddbc
Carsten Schneider <carsten.schneider@shopgate.com>
2019-06-27 11:19:19 +0200

    added track.js script

--- frontend/subscriptions/index.js
+++ frontend/subscriptions/index.js
@@ -2,14 +2,26 @@ import { appDidStart$ } from '@shopgate/pwa-common/streams/app';
 import { cartReceived$ } from '@shopgate/pwa-common-commerce/cart/streams';
 import { fetchBoltCartToken } from '../actions';
 import injectBoltConnect from '../helpers/injectBoltConnect';
+import getConfig from '../helpers/getConfig';
+
+const config = getConfig();
 
 /**
  * @param {Function} subscribe subscriber function
  */
 export default (subscribe) => {
   subscribe(appDidStart$, () => {
+    const script = document.createElement('script');
+    script.id = 'bolt-track';
+    script.type = 'text/javascript';
+    script.src = `https://${config.cdnUrl}.com/track.js`;
+    script.setAttribute('data-publishable-key', config.boltPublishableKey);
+    const parent = document.getElementsByTagName('head')[0];
+    parent.appendChild(script);
+
     injectBoltConnect();
   });
+
   subscribe(cartReceived$, ({ dispatch }) => {
     dispatch(fetchBoltCartToken());
   });
