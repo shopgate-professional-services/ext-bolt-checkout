7457842dbe02e128d83340475cd94c1b5cb39e1d
Aaron Martin <aaron.martin@shopgate.com>
2020-02-11 11:57:26 -0600

    CCP-1966: added willUpdate property of cartToken redux state to be used to disable bolt checkout button during cart update

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,6 +5,8 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](http://keepachangelog.com/) and this project adheres to [Semantic Versioning](http://semver.org/).
 
 ## [Unreleased]
+### Fixed
+- Disabling bolt checkout during cart item update
 
 ## [1.1.3] - 2020-01-22
 ### Fixed
--- frontend/reducers/boltCartToken.js
+++ frontend/reducers/boltCartToken.js
@@ -1,3 +1,4 @@
+import { UPDATE_PRODUCTS_IN_CART } from '@shopgate/engage/cart';
 import {
   RECEIVE_BOLT_CART_TOKEN,
   REQUEST_BOLT_CART_TOKEN,
@@ -14,24 +15,34 @@ const boltTokenReducer = (
   state = {
     cartToken: null,
     isFetching: false,
+    willUpdate: false,
   },
   action
 ) => {
   switch (action.type) {
+    case UPDATE_PRODUCTS_IN_CART: {
+      return {
+        ...state,
+        willUpdate: true,
+      };
+    }
     case REQUEST_BOLT_CART_TOKEN:
       return {
         ...state,
         isFetching: true,
+        willUpdate: false,
       };
     case RECEIVE_BOLT_CART_TOKEN:
       return {
         ...action.boltCartToken,
         isFetching: false,
+        willUpdate: false,
       };
     case ERROR_BOLT_CART_TOKEN:
       return {
         ...state,
         isFetching: false,
+        willUpdate: false,
       };
     default:
       return state;
